25787c608ba61aa5088e3c9ff7794024f9743452
Rene Niehus <rene.niehus@gmx.de>
2020-02-18 07:08:42 -0500

    Update load_regression_plot.R

--- load_regression_plot.R
+++ load_regression_plot.R
@@ -23,26 +23,62 @@ boot_pi <- function(model, pdata, n, p) {
                 return(data.frame(pred = predict(model, newdata = pdata, type = "response"), lower = boot_ci[, 1], upper = boot_ci[, 2]))
 }
 
-# load dataset
-load(file="~/Desktop/Corona/epidata.Rdata")
-df <- df %>% mutate(Wuhan_airtravel_1=Wuhan_airtravel+1) # add a small constant to travel volume
+# load data for cases + flight volume
+load(file="~/Desktop/Corona/epidata.Rdata") # WRONG DATA FOR SWEDEN
+#
+# load ghs data
+df_ghs <- read_csv(file="~/Desktop/Corona/ghs_index.csv",col_names = F) %>% 
+                rename(Country=X1,ghs=X2) %>% 
+                left_join( tibble(Country=c("United States",
+                                            "United Kingdom",
+                                            "United Arab Emirates",
+                                            "Sweden",
+                                            "New Zealand",
+                                            "South Korea"),
+                                  Country_new=c("US",
+                                                "UK",
+                                                "UAE",
+                                                "Sweeden",
+                                                "New Zeland",
+                                                "Rep  Korea") ) ) %>% 
+                mutate( Country=ifelse(is.na(Country_new),Country,Country_new) ) %>% select(-Country_new)
+# bind datasets 
+df_all0 <- full_join( df_ghs,df, by="Country" ) %>% mutate( Cases_lm=replace_na(Cases_lm,replace=0),
+                                                           Wuhan_airtravel=replace_na(Wuhan_airtravel,replace=0)) 
+# settings
+min_airtravel <- 1.5 
+ghs_quantile <- 0.75
+# setting air travel volume to min_airtravel
+# giving binary label according to GHS index
+ghs_limit <- df_all0$ghs %>% quantile(ghs_quantile)  
+df_all <- df_all0 %>% 
+                mutate( Wuhan_airtravel_gamma=ifelse(Wuhan_airtravel<3,
+                                                     min_airtravel,
+                                                     Wuhan_airtravel) ) %>% 
+                mutate( ghs_high=as.numeric(ghs>ghs_limit) )
+
+
 
 # fit the data
-mdl <- glm(Cases_lm ~ 1+offset( log(Wuhan_airtravel_1) ), data = df, family=poisson( link="log" ) )
-new_data <- df
-
+df_fit <- df_all %>% filter(ghs_high==1) # rnows=49
+mdl <- glm(Cases_lm ~ 1+offset( log(Wuhan_airtravel_gamma) ), data = df_fit, family=poisson( link="log" ) )
+new_data <- df_all
 # compute the prediction intervals
 PI<-boot_pi(mdl, new_data, 50000, 0.95)
 
 # visualisation
-df$fit<-PI$pred
-df$lwr<-PI$lower
-df$upr<-PI$upper
-ggplot(df)+
-                geom_point(aes(x=Wuhan_airtravel,y=Cases_lm),size=3, alpha=1)+
-                geom_line(aes(x=Wuhan_airtravel,y=fit),size=1, colour="grey80")+
-                geom_line(aes(x=Wuhan_airtravel,y=lwr),size=1, colour="darkred", linetype=2)+
-                geom_line(aes(x=Wuhan_airtravel,y=upr),size=1, colour="grey80", linetype=2)+
-                geom_text(aes(x=Wuhan_airtravel,y=Cases_lm, label=Country), hjust=0.5 , vjust=-0.8, size=3.8)+
-                ylab("Reported cases")+
-                xlab("Average number passanger/day")
+df_all$fit<-PI$pred
+df_all$lwr<-PI$lower
+df_all$upr<-PI$upper
+(p1 <- ggplot(df_all)+
+                geom_vline(xintercept = 0, color="lightgrey") +
+                geom_point(aes(x=Wuhan_airtravel,y=Cases_lm,color=as.factor(ghs_high)),
+                           size=2, alpha=1, show.legend = F )+
+                geom_line(aes(x=Wuhan_airtravel,y=fit),size=1, colour="black")+
+                stat_smooth(aes(x=Wuhan_airtravel,y=lwr),size=1, colour="grey80", linetype=2,
+                            method = lm, formula = y ~ poly(x, 2), se = FALSE)+
+                stat_smooth(aes(x=Wuhan_airtravel,y=upr),size=1, colour="grey80", linetype=2,
+                            method = lm, formula = y ~ poly(x, 2), se = FALSE)+
+                labs(x="Daily air travel volume (number of passengers per day)",
+                     y="Number of imported cases of 2019-nCoV"))
+
